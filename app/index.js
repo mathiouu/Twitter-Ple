0a8616b67dde6d5ede24273e47ec80403ed5a2e8
smelezan <sebastien.melezan972@gmail.com>
2021-01-16 19:12:44 +0100

    ajout client

--- app/index.js
+++ app/index.js
@@ -4,9 +4,36 @@ const client = hbase({
     port: 8080
   });
 
-client.table('seb-mat-hashtagsTriplets' ).row('row1').get('hashtags', function(err, cell) 
+const containsUser = ([k,v]) =>{
+  return v.users.some((tuple)=> tuple._1 === "x03040x" ) ;
+};
+let mapUser = new Map();
+
+client.table('testSmelezan' ).row('row*').get('hashtags', function(err, cell) 
   {
-    console.log(cell);
+    cell.forEach(line =>{
+      let currentHashtag ="";
+      if(line.column=== 'hashtags:hashtags'){
+        let value = mapUser.get(line.key);
+        if(value===undefined){
+          value = {hashtags: line['$']}
+        }
+        else{
+          value.hashtags = line['$'];
+        }
+        mapUser.set(line.key, value);
+      }
+      if(line.column=== 'hashtags:users'){
+        let value = mapUser.get(line.key);
+        let tmp = line['$'];
+        value.users = JSON.parse(tmp);
+        mapUser.set(line.key, value);
+      }
+    });
+    const res = [...mapUser].filter(containsUser);
+    res.forEach(([k,v]) => {
+      console.log(v.users);
+    })
     console.log();
   }
 );
