5c360ec08915621732a08b461baec573a599f554
mathieu <mathieu.duban@gmail.com>
2021-01-25 22:35:57 +0100

    update controllers q2

---
+++ app/server/api/userQ2/userHashtags.js
@@ -0,0 +1,6 @@
+module.exports = {
+    tableName : 'seb-mat-userHashtags',
+    columnName : 'userHashtags',
+    user : 'userHashtags:user',
+    hashTags : 'userHashtags:hashTags',
+}
---
+++ app/server/api/userQ2/userNbTweet.js
@@ -0,0 +1,6 @@
+module.exports = {
+    tableName : 'seb-mat-userNbTweet',
+    columnName : 'userNbTweet',
+    user : 'userNbTweet:user',
+    times : 'userNbTweet:times',
+}
